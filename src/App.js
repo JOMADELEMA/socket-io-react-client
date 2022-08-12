7636463b84949295dee611d71ffb7e3b56b035cb
JOMADELEMA <jomadelema@gmail.com>
2022-08-11 22:59:30 -0600

    se preparó para enviar y recibir mensajes desde servidor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import "./App.css";
 import { io } from "socket.io-client";
+import { useState, useEffect } from "react";
 
 const socket = io('http://localhost:4000')
 
 function App() {
+  const [message, setMessage] = useState("")
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    socket.emit('message', message);
+    setMessage('')
+  }
+
+  useEffect(()=>{
+
+    const receiveMessage = message => {
+      console.log(message);
+    }
+
+    socket.on('message', receiveMessage)
+    return ()=> {
+      socket.off('message', receiveMessage)
+    }
+  },[])
+  
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={e=>setMessage(e.target.value)} value={message}/> 
+        <button>send</button>
+        </form>
     </div>
   );
 }
